1f561c4e77ffa84a8b28e3463067e8756727f989
ibrahim <ibrahimimmu3533@gmail.com>
2023-04-23 22:16:05 +0200

    update

---
+++ main.py
@@ -0,0 +1,10 @@
+# ptyhon tutorial 
+# ptyhon is  a popular programming langugage
+# python can be used on a server to create web applications
+
+# lerning  by example 
+
+print("hello there")
+
+# python file handling 
+# python  database handling

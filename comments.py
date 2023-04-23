de564043d210208be8c19bcc8cb1d9684e493211
ibrahim <ibrahimimmu3533@gmail.com>
2023-04-23 22:30:34 +0200

    update

---
+++ comments.py
@@ -0,0 +1,26 @@
+# this is a comment
+print("COMMENT") 
+
+
+print("hi") #this is a comment 
+
+
+# print("hello there")
+print("cheers, viktor")
+# multilne commments
+
+
+# this is a comment 
+# written in 
+# more than just one line 
+
+
+
+"""
+this is the way 
+of comment written in one 
+line
+"""
+
+
+print("this is the way")
--- main.py
+++ main.py
@@ -8,3 +8,24 @@ print("hello there")
 
 # python file handling 
 # python  database handling
+
+# python snytax
+
+if 5>2:
+  print('five is greater than two')
+if 5>4:
+    print("five is greater than four")
+    print('hello python')
+
+
+    # python variables
+    
+    x = 5
+    y = "hello world"
+    print(x)
+    print(y)
+
+
+    # comments
+    # this is a comment
+    print("#")

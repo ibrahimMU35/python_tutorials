c74906784184867eac5912c0de010fe8f7b98821
ibrahim <ibrahimimmu3533@gmail.com>
2023-04-23 22:45:41 +0200

    update

---
+++ variables.py
@@ -0,0 +1,41 @@
+# creating variables
+
+x = 5
+y = "jhon"
+print(x)
+print(y)
+
+x = 4 # x is type of int 
+x = "sally" # x is now type of str
+print(x)
+
+
+# casting
+
+x = str(3) # x will be '3'
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+
+
+print(x)
+print(y)
+print(z)
+
+
+# single or double quotes?
+
+
+x = "jhon"
+# x is the same as
+x = 'jhon'
+print(x)
+
+
+
+# case-sensitive
+a = 4
+A = "sally"
+# A wil not overwrite a 
+print(a)
+print(A)
+

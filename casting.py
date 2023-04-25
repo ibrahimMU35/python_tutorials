8285747436ca81a0e7c0d9bc4358082451b34d94
ibrahim <ibrahimimmu3533@gmail.com>
2023-04-25 20:51:39 +0200

    update

---
+++ casting.py
@@ -0,0 +1,58 @@
+# Specify a Variable Type
+# int()
+# float()
+# str()
+
+# integers
+
+x = int(2)
+y = int(2.8)
+z = int('3')
+
+print(x)
+print(type(x))
+
+print(y)
+print(type(y))
+
+print(z)
+print(type(z))
+
+
+# floats
+a = float(2)
+print(a)
+print(type(a))
+
+b = float(4.5)
+print(b)
+print(type(b))
+
+c = float("5.6")
+print(c)
+print(type(c))
+
+d = float("3")
+print(d)
+print(type(d))
+
+# strings
+
+x = str("s1")
+print(x)
+print(type(x))
+
+
+y = str(3)
+print(y)
+print(type(y))
+
+z = str(3.0)
+print(z)
+print(type(z))
+
+last = str(45)
+print(last)
+print(type(last))
+
+

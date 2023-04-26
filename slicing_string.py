9d0b245d1254f0edd316928a0db1c4fdfa5d5a39
ibrahim <ibrahimimmu3533@gmail.com>
2023-04-26 20:18:08 +0200

    update

---
+++ slicing_string.py
@@ -0,0 +1,28 @@
+# Python - Slicing Strings
+# Get the characters from position 2 to position 5 (not included):
+b = "hello world"
+
+print(b[2:5])
+
+# Note: The first character has index 0.
+
+
+
+# Slice From the Start
+b = "hello world"
+print(b[:5])
+
+
+
+
+# slice to end
+b = "Hello, World!"
+print(b[2:])
+
+
+# Negative Indexing
+
+# Use negative indexes to start the slice from the end of the string:
+
+b = "hello world"
+print(b[-5:-2])

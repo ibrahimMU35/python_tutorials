ff600b61c64c604312d37c6224e0af2563b7091f
ibrahim <ibrahimimmu3533@gmail.com>
2023-04-26 21:50:25 +0200

    update

---
+++ modify._string.py
@@ -0,0 +1,24 @@
+# Upper Case
+a = "hello world"
+print(a.upper())
+
+
+# lower case
+a = "HI THERE"
+print(a.lower())
+
+# remove whitespace
+
+a = " hello world    "
+print(a.strip())
+
+# replace string
+
+a = "hello world"
+print(a.replace("h","J"))
+
+
+# split string
+a = "hello, world"
+print(a.split(",")) 
+

23eb0f498ddcd469b106063b69140568e531e59e
ibrahim <ibrahimimmu3533@gmail.com>
2023-04-27 18:32:06 +0200

    update

---
+++ string_escape_characters.py
@@ -0,0 +1,45 @@
+# An escape character is a backslash \ followed by the character you want to insert.
+# txt = "We are the so-called "Vikings" from the north."
+
+# single quote \'
+
+
+txt = 'it\'s allright'
+print(txt)
+
+
+
+# backslash \\
+txt = " this will be insert one \\ backslash"
+print(txt)
+
+#new line \n
+
+txt = 'this is the \n newline'
+print(txt)
+
+
+ 
+# carriage return \r
+abc = "Hello\rWorld"
+print(abc)
+
+# tab \t
+
+txt = "hello\tworld" 
+print(txt)
+
+# backspace \b
+txt = "hello\bworld"
+print(txt)
+
+
+# Form Feed \f
+
+
+# Octal value \ooo
+txt = "\110\145\154\154\157" # Hello 
+print(txt) 
+# Hex value \xhh
+txt = "\x48\x65\x6c\x6c\x6f" # Hello
+print(txt) 
